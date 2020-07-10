63b85b5c17775fd939ee495080160356c3c151b0
sarab <sarabjeet.mannu.singh@gmail.com>
2020-07-10 02:14:57 -0300

    July9_2020 Max-Width of binary tree

---
+++ July9_2020.java
@@ -0,0 +1,39 @@
+// Max width of binary tree - level order traversal woth hack
+
+
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) 
+    {
+		 if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        int max = 1, leftmost = 1, rightmost = 1;
+        root.val = 1;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == 0) {
+                    leftmost = cur.val;
+                }
+                if (i == size - 1) {
+                    rightmost = cur.val;
+                }
+                if (cur.left != null) {
+                    cur.left.val = cur.val * 2;
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    cur.right.val = cur.val * 2 + 1;
+                    queue.offer(cur.right);
+                }
+            }
+            max = Math.max(max, rightmost - leftmost + 1);
+
+        }
+        return max;
+	}
+}
